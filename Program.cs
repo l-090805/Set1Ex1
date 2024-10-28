e1170180879664b50ae681386d807fb3998a9fe0
falau <falau@LENOVO>
2024-10-28 10:18:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Set1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti primul numar: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduceti al 2-lea numar: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double x = -b / a;
+
+
+            Console.WriteLine($"Rezultatul ecuatiei {a}*x+{b}=0 este {x}");
+        }
+    }
+}
